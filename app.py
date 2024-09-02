4b2bb69baf94f22443ae1f436d865d243cd7dd7b
Chris <chrismdowning@gmail.com>
2024-09-02 11:48:37 -0700

    Removed old, unused code that had been commented out.

--- app.py
+++ app.py
@@ -10,30 +10,7 @@ app = Flask(__name__)
 file_name = "Output.csv"
 df = pd.read_csv("Listed_Venues.csv")
 
-"""
-# create an empty dictionary to hold the show information
-shows_tonight = {
-    "venue": [],
-    "show": [],
-    "date": [],
-}
-
-
-# make the API calls to get show information and store it in the shows_tonight dictionary
-for index, row in df.iterrows():
-    venue, band, date = get_shows(row["Venue Name"], row["vID"])
-    shows_tonight["venue"].append(venue)
-    shows_tonight["show"].append(band)
-    shows_tonight["date"].append(date)
-    sleep(0.09)
-"""
-
-"""
-# declare the OLD global variable
-SHOW_INFO = list(zip(shows_tonight["venue"], shows_tonight["show"], shows_tonight["date"]))
-"""
-
-# declare the NEW global variable for storing show info with new structure
+# declare the global variable for storing show info with new structure
 SHOWS = []
 
 # Test loop for restructuring how the information is stored
@@ -41,16 +18,6 @@ for index, row in df.iterrows():
     venue, band, date = get_shows(row["Venue Name"], row["vID"])
     SHOWS.append([venue, band, date])
     sleep(0.09)
-
-"""
-# convert the dictionary to a pandas dataframe
-df_show_data = pd.DataFrame(shows_tonight)
-df_show_data.to_csv(file_name, index=False)
-
-# test code to output and verify the contents that get_shows() has returned
-print(df_show_data.to_string())
-print(shows_tonight)
-"""
 
 
 @app.route("/")
